529efbe5cd8f570919c9d04df47b2f3a908ddbbf
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2022-01-22 22:48:12 +0500

    Implement validation of a location
    Used pragmarx/countries package for getting a country code in ISO3 format for a given country name.

--- app/Repository/Eloquent/MigrateCustomerRepository.php
+++ app/Repository/Eloquent/MigrateCustomerRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository\Eloquent;
 use App\Models\Customer;
 use App\Repository\MigrateCustomerRepositoryInterface;
 use Exception;
+use PragmaRX\Countries\Package\Countries;
 use Validator;
 
 class MigrateCustomerRepository implements MigrateCustomerRepositoryInterface
@@ -12,11 +13,13 @@ class MigrateCustomerRepository implements MigrateCustomerRepositoryInterface
 
     public function migrateDataToCustomers($file)
     {
-        // TODO: Implement validation of a location
         // TODO: Generate an Excel report file which contains validation errors
         try{
             if (!file_exists($file))
                 throw new Exception("File $file doesn't exist. Please check and provide a valid path to your CSV file.");
+
+            $countries = new Countries();
+            $countryList = $countries->all()->pluck('iso_a3', 'name_en')->toArray();
 
             $validationRules = array(
                 'email'=>'required|email:rfc,dns',
@@ -28,12 +31,14 @@ class MigrateCustomerRepository implements MigrateCustomerRepositoryInterface
                 while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
                     if ($rowNumber > 1){
                         $fullname = (isset($data[1])) ? explode(' ', $data[1]) : array();
+
                         $row = array(
                             'name'=>(isset($fullname[0])) ? $fullname[0] : '',
                             'surname'=>(isset($fullname[1])) ? $fullname[1] : '',
                             'email'=>(isset($data[2])) ? $data[2] : '',
                             'age'=>(isset($data[3])) ? $data[3] : '',
-                            'location'=>(isset($data[4])) ? $data[4] : '',
+                            'location'=>(isset($data[4]) && !empty($data[4]) && isset($countryList[$data[4]])) ? $data[4] : 'Unknown',
+                            'country_code'=>(isset($data[4]) && !empty($data[4]) && isset($countryList[$data[4]])) ? $countryList[$data[4]] : null
                         );
                         $validator = Validator::make($row, $validationRules);
                         if ($validator->fails()) {
@@ -51,7 +56,8 @@ class MigrateCustomerRepository implements MigrateCustomerRepositoryInterface
                                     'surname'=>$row['surname'],
                                     'email'=>$row['email'],
                                     'age'=>$row['age'],
-                                    'location'=>$row['location']
+                                    'location'=>$row['location'],
+                                    'country_code'=>$row['country_code']
                                 ],
                                 []
                             );
